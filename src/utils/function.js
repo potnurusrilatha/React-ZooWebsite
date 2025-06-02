58260fe62ab87aa0499f38dd67a92b0873e2fd87
Srilatha <119068383+potnurusrilatha@users.noreply.github.com>
2025-06-02 20:19:26 +0200

    changed utility imagr function0

--- src/utils/function.js
+++ src/utils/function.js
@@ -6,9 +6,11 @@ export const getIconUrl = (icons) => {
     return new URL(`../assets/icons/${icons}`,import.meta.url).href;
 }
 
-export const getImageUrl = (images) => {
-    return new URL(`../assets/images/${images}`,import.meta.url).href;
-}
+// export const getImageUrl = (images) => {
+//     return new URL(`../assets/images/${images}`,import.meta.url).href;
+// }
+
+export const getImageUrl = (image) => `/images/${image}`;
 
 export const getVideoUrl = (video) => {
     return new URL(`../assets/video/${video}`,import.meta.url).href;
