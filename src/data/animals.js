e3c2186938f6a415444de5a4ef091b5c86571e4a
Srilatha <119068383+potnurusrilatha@users.noreply.github.com>
2025-06-02 19:03:32 +0200

    fixed image

--- src/data/animals.js
+++ src/data/animals.js
@@ -18,12 +18,12 @@ function Animals(groupName, name, lifeSpan, food, description, length, weight, p
 
 let mammalEchidna = new Animals("Mammals", "Echidna", 50, ["insects such as ants and termites", "beetle larvae", "worms"],
     "Echidnas, also called spiny anteaters, are walking contradictions. They are mammals, but they lay eggs. They are often classified as long- or sort-beaked, but don't have beaks at all, in the traditional sense; they have fleshy noses that can be either on the long side or rather short. They don't really look like true anteaters (Myrmecophaga tridactyla), either, and they are not closely related to them. They are spiny, though; their bodies are covered with hollow, barbless quills. Echidnas are monotremes, egg-laying mammals. The only other living monotreme is the platypus.",
-    76, 10, "Throughout Austalia", "./images/echidna.jpg", 'Mammals', './Mammals/index.jsx','echidna.jpg','echidna.png');
+    76, 10, "Throughout Austalia", "./images/chidna.jpg", 'Mammals', './Mammals/index.jsx','echidna.jpg','echidna.png');
 
 
 let mammalTasmanianDevil  = new Animals("Mammals", "Tasmanian Devil", 5, ["A predator", "eat meat from other animals such as wallabies and wombats"],
     "The Tasmanian devil (Sarcophilus harrisii) (palawa kani: purinina)[3] is a carnivorous marsupial of the family Dasyuridae. Until recently, it was only found on the island state of Tasmania, but it has been reintroduced to New South Wales in mainland Australia, with a small breeding population. The size of a small dog, the Tasmanian devil became the largest carnivorous marsupial in the world, following the extinction of the thylacine in 1936. It is related to quolls, and distantly related to the thylacine. It is characterised by its stocky and muscular build, black fur, pungent odour, extremely loud and disturbing screech, keen sense of smell, and ferocity when feeding. The Tasmanian devil's large head and neck allow it to generate among the strongest bites per unit body mass of any extant predatory land mammal. It hunts prey and scavenges on carrion.",
-    70, 10, "Tasmania",  "./images/tasmanian devil.jpg", 'Mammals', './Mammals/index.jsx', 'tasmanian devil.jpg', 'tasdevil.png');
+    70, 10, "Tasmania",  "./images/tasmaniandevil.jpg", 'Mammals', './Mammals/index.jsx', 'tasmanian devil.jpg', 'tasdevil.png');
 
 
 let mammalquokka  = new Animals("Mammals", "Quokka", 10, ["Plant eaters", " shrubs", "grasses"],
